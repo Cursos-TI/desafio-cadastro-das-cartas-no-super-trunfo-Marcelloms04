be940d1f0e69fc5afbb2fd6b5901350afac9c51d
Marcelloms04 <marcellozebra@gmail.com>
2025-03-21 18:35:15 +0000

    CartasSuperTrunfo-NívelMestre-fase3 adicionei meu código fonte aqui após concluir no meu codenpass pessoal

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // variaveis dos paises 
+    char nomepais1[30] = "alemanha", nomepais2[30] = "japão";
+    unsigned long populacao1 = 83129285, populacao2 = 125410000;
+    double area1 = 357022.0, area2 = 377975.0;
+    double pib1 = 4.5, pib2 = 5.1; 
+    int pontosTuristicos1 = 20, pontosTuristicos2 = 30;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    // calculo da densidade populacional e pib per capita
+    double densidadePop1 = populacao1 / area1;
+    double densidadePop2 = populacao2 / area2;
+    double pibPerCapita1 = (pib1 * 1e12) / populacao1;
+    double pibPerCapita2 = (pib2 * 1e12) / populacao2;
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
+    // calculo do super poder 
+    float superpoder1 = populacao1 + area1 + (pib1 * 1e12) + pontosTuristicos1 + pibPerCapita1 + (1 / densidadePop1);
+    float superpoder2 = populacao2 + area2 + (pib2 * 1e12) + pontosTuristicos2 + pibPerCapita2 + (1 / densidadePop2);
+    
+    // resultados
+    printf("\ncomparaçao de cartas:\n");
+    printf("população: %s venceu (%d)\n", (populacao1 > populacao2) ? nomepais1 : nomepais2, (populacao1 > populacao2) ? populacao1 : populacao2);
+    printf("area: %s venceu (%d)\n", (area1 > area2) ? nomepais1 : nomepais2, (area1 > area2));
+    printf("pib: %s venceu (%d)\n", (pib1 > pib2) ? nomepais1 : nomepais2, (pib1 > pib2));
+    printf("pontos turísticos: %s venceu (%d)\n", (pontosTuristicos1 > pontosTuristicos2) ? nomepais1 : nomepais2, (pontosTuristicos1 > pontosTuristicos2));
+    printf("densidade populacional: %s venceu (%d)\n", (densidadePop1 < densidadePop2) ? nomepais1 : nomepais2, (densidadePop1 < densidadePop2));
+    printf("pib per capita: %s venceu (%d)\n", (pibPerCapita1 > pibPerCapita2) ? nomepais1 : nomepais2, (pibPerCapita1 > pibPerCapita2));
+    printf("super Poder: %s venceu (%d)\n", (superpoder1 > superpoder2) ? nomepais1 : nomepais2, (superpoder1 > superpoder2));
+    
     return 0;
 }
